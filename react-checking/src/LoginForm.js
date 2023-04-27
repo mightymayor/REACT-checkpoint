0a5d4597c59ed8a988636bb7443069d8379703a0
faraowait <arafabubakar4@gmail.com>
2023-04-27 11:33:42 +0100

    react

---
+++ react-checking/src/LoginForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './LoginForm.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Loginform() {
+  return (
+    <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
+      <div className="login ">
+        <h2 className='mb-3'>Login Form</h2>
+        <form>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input type='email'></input>
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input type='password'></input>
+        </div>
+        <div className='form-group'>
+          <input type='checkbox'></input>
+          <label htmlFor='check'>Remember me</label>
+        </div>
+        <button className='btn btn-primary'>SIGN IN</button>
+        </form>
+      </div>
+    </div>
+
+  )
+}
+
+export default Loginform
